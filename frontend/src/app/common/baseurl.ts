fd5da5dbbc8d16a8778a51302b34e230ebcc0944
Angel Rey <afreydev@gmail.com>
2019-06-24 17:33:02 -0400

    cambio puerto

--- frontend/src/app/common/baseurl.ts
+++ frontend/src/app/common/baseurl.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'http://aplicacion.com:8080/testWar/';
+export const baseURL = 'http://aplicacion.com:8081/testWar/';
 export const httpBaseURL = 'http://aplicacion.com:4200';
